1ce63b8d674dc740ddf15088e0e019fbfd3526ed
Brian Mc Alister <brian@mcalister.ch>
2021-01-26 19:09:14 +0100

    fixed bug

--- Source/FolioReaderKit.swift
+++ Source/FolioReaderKit.swift
@@ -165,7 +165,7 @@ extension FolioReader {
         let readerContainer = FolioReaderContainer(withConfig: config, folioReader: self, epubPath: epubPath, unzipPath: unzipPath, removeEpub: shouldRemoveEpub)
         self.readerContainer = readerContainer
         if (fullScreen) {
-            self.readerContainermodalPresentationStyle = .fullScreen
+            self.readerContainer.modalPresentationStyle = .fullScreen
         }
         parentViewController.present(readerContainer, animated: animated, completion: nil)
         addObservers()
